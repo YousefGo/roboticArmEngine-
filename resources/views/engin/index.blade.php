d22cb05c25f9600019b6043f96a6daa1fd340528
yousef <yousef0sarkar@gmail.com>
2021-06-22 06:05:47 +0300

    edit table sytle

--- resources/views/engin/index.blade.php
+++ resources/views/engin/index.blade.php
@@ -24,7 +24,7 @@
 </head>
 
 <body>
-
+  
    <div class="continer-fulid">
 
 
@@ -54,7 +54,7 @@
 
    
 
-
+   
 
      
 
@@ -69,208 +69,151 @@
                     </i>
                 </span>
 
-          
+     
 
 
         </div>
-      
-
         <form action="{{route('store.engine')}}" method="Post"  class="text-center" onload="myFunction()">
-            @csrf
-        <div class="row ">
-            <div class="col-md-2 col-12"></div>
-            <div class="col-md-2  col-12 ">
-                <label class="mt-3 og  w-75  bg-dark"> <i class="fas fa-cog "> &nbsp  {{__('ui.en1')}}</i>
-                </label>
-            </div>
-            <div class="col-md-4 col-12 ">
-                <input type="range" step='0.1' name="engineN1" class="pointss w-100 bg-dark "  value={{$en->engineN1}}
-                    oninput="changevalue3(0); " min="0" max="180">
-    
-            </div>
-    
-            <div class="col-md-2 col-12 ">
-                <label class="ps3 og mt-3  w-75  bg-primary  text-light" value='{{$en->engineN1}}'>{{$en->engineN1}}</label>
-    
-            </div>
-            <div class="col-md-2"></div>
-        </div>
-      
-
-       
-        <div class="row ">
-
-
-        <div class="col-md-2"></div>
-        <div class="col-md-2  col-12">
-            <label class="mt-3 og  w-75  bg-dark"> <i class="fas fa-cog "> &nbsp  {{__('ui.en2')}}</i>
-            </label>
-        </div>
-        <div class="col-md-4 col-12">
-            <input type="range" name="engineN2" class="pointss w-100 bg-dark" name="points" value={{$en->engineN2}}
-                oninput="changevalue3(1); " min="0" max="180">
-
-        </div>
-
-        <div class="col-md-2 col-12 ">
-            <label class="ps3 og mt-3  w-75  bg-primary   text-light"value={{$en->engineN2}}>{{$en->engineN2}}</label>
-
-        </div>
-        <div class="col-md-2"></div>
-
-        </div>
-
-
-        <div class="row ">
-
-
-        <div class="col-md-2"></div>
-        <div class="col-md-2  col-12">
-            <label class="mt-3 og  w-75  bg-dark"> <i class="fas fa-cog "> &nbsp {{__('ui.en3')}}</i>
-            </label>
-        </div>
-        <div class="col-md-4 col-12">
-            <input type="range" name="engineN3" class="pointss w-100 bg-dark" name="points" value={{$en->engineN3}}
-                oninput="changevalue3(2); " min="0" max="180">
-
-        </div>
-
-        <div class="col-md-2 col-12 ">
-            <label class="ps3 og   mt-3  w-75 bg-primary   text-light" value={{$en->engineN3}}>{{$en->engineN3}}</label>
-
-        </div>
-        <div class="col-md-2"></div>
-        
-    </div>
-
-         <input type="hidden" name="lang" value='{{__('ui.lang')}}'/>
-        <div class="row ">
-    
-        <div class="col-md-2"></div>
-        <div class="col-md-2  col-12">
-            <label class="mt-3 og  w-75  bg-dark"> <i class="fas fa-cog "> &nbsp {{__('ui.en4')}}</i>
-            </label>
-        </div>
-        <div class="col-md-4 col-12">
-            <input type="range" name="engineN4" class="pointss w-100 bg-dark" name="points" value={{$en->engineN4}}
-                oninput="changevalue3(3); " min="0" max="180">
-
-        </div>
-
-        <div class="col-md-2 col-12 ">
-            <label class="ps3 og mt-3  w-75  bg-primary   text-light" value={{$en->engineN4}}>{{$en->engineN4}}</label>
-
-        </div>
-        <div class="col-md-2"></div>
-        </div>
-
-        <div class="row ">
-
-        <div class="col-md-2"></div>
-        <div class="col-md-2  col-12">
-            <label class="mt-3 og  w-75  bg-dark"> <i class="fas fa-cog "> &nbsp {{__('ui.en5')}}</i>
-            </label>
-        </div>
-        <div class="col-md-4 col-12">
-            <input type="range" name="engineN5" class="pointss w-100 bg-dark" name="points" value={{$en->engineN5}}
-                oninput="changevalue3(4); " min="0" max="180">
-
-        </div>
-
-        <div class="col-md-2 col-12 ">
-            <label class="ps3 og mt-3  w-75  bg-primary   text-light" value={{$en->engineN5}}>{{$en->engineN5}}</label>
-
-        </div>
-        <div class="col-md-2"></div>
-        </div>
-
-
-        <div class="row ">
-
-        <div class="col-md-2"></div>
-        <div class="col-md-2  col-12">
-            <label class="mt-3 og  w-75  bg-dark"> <i class="fas fa-cog "> &nbsp {{__('ui.en6')}}</i>
-            </label>
-        </div>
-        <div class="col-md-4 col-12">
-            <input type="range" name="engineN6" class="pointss w-100 bg-dark" name="points" value="{{$en->engineN6}}"
-                oninput="changevalue3(5); " min="0" max="180">
-
-        </div>
-
-        <div class="col-md-2 col-12 ">
-            <label class="ps3 og mt-3  w-75  bg-primary   text-light" value={{$en->engineN6}}>{{$en->engineN6}}</label>
-
-        </div>
-        <div class="col-md-2"></div>
-        </div>
-      
-    
-            @if (session()->exists('message'))
-        
-            <div class="row">
-                <h4 class="text-primary mx-auto"> 
-                    {{__('ui.updated')}}    
-                </h4>
-            </div>
-
+          @csrf
+         <div class="container">
+            <table class="table black  text-center  ">
+                <tr>
+              <th class="text-light"> {{__('ui.thd1')}}</th>
+              <th>  
                   
-              @endif
+                 
+                @if (session()->exists('message'))
+        
+                <div class="row">
+                    <h4 class="text-primary mx-auto"> 
+                        {{__('ui.updated')}}    
+                    </h4>
+                </div>
+    
+                      
+                  @endif
+    
+    
+                  @if (session()->exists('msg'))
+                  <div class="row">
+              
+                  <h4 class="text-warning mx-auto mt-2"> 
+    
+                      {{__('ui.run')}} {{$en['status']}}    
+                  </h4>
+                </div>
+                    
+                @endif
+            
+              </th>
+              <th class="text-light">{{__('ui.thd3')}}  </th>
+                </tr>
+            
+               <td>  <label class=" og  w-100 "> <i class="fas fa-cog "> &nbsp  {{__('ui.en1')}}</i> </label></td>
+               <td>   <input type="range" step='0.1' name="engineN1" class="pointss   "  value={{$en->engineN1}}
+                oninput="changevalue3(0); " min="0" max="180" ></td>
+               <td>                 <label class="ps3 og   w-100    text-light" value='{{$en->engineN1}}'>{{$en->engineN1}}</label>
+               </td>
+    
+      
+                </tr>
+
+              <tr>
+              <td> <label class=" og  w-100  "> <i class="fas fa-cog "> &nbsp  {{__('ui.en2')}}</i>  </td>
+              <td>  <input type="range" name="engineN2" class="pointss w-100 " name="points" value={{$en->engineN2}}
+                oninput="changevalue3(1); " min="0" max="180">  </td>
+              <td>             <label class="ps3 og   w-100     text-light"value={{$en->engineN2}}>{{$en->engineN2}}</label>
+              </td>
+
+              </tr>
 
 
-              @if (session()->exists('msg'))
-              <div class="row">
+              <tr>
+                <td> <label class=" og  w-100  "> <i class="fas fa-cog "> &nbsp  {{__('ui.en3')}}</i>  </td>
+                <td>  <input type="range" name="engineN3" class="pointss w-100 "  value={{$en->engineN3}}
+                  oninput="changevalue3(2); " min="0" max="180">  </td>
+                <td>             <label class="ps3 og   w-100     text-light"value={{$en->engineN3}}>{{$en->engineN3}}</label>
+                </td>
+  
+                </tr>
+
+
+                <tr>
+                    <td> <label class=" og  w-100  "> <i class="fas fa-cog "> &nbsp  {{__('ui.en4')}}</i>  </td>
+                    <td>  <input type="range" name="engineN4" class="pointss w-100 "  value={{$en->engineN4}}
+                      oninput="changevalue3(3); " min="0" max="180">  </td>
+                    <td>             <label class="ps3 og   w-100     text-light"value={{$en->engineN4}}>{{$en->engineN4}}</label>
+                    </td>
+      
+                    </tr>
+
+
+
+                    <tr>
+                        <td> <label class=" og  w-100  "> <i class="fas fa-cog "> &nbsp  {{__('ui.en5')}}</i>  </td>
+                        <td>  <input type="range" name="engineN5" class="pointss w-100 "  value={{$en->engineN5}}
+                          oninput="changevalue3(4); " min="0" max="180">  </td>
+                        <td>             <label class="ps3 og   w-100     text-light"value={{$en->engineN5}}>{{$en->engineN5}}</label>
+                        </td>
           
-              <h4 class="text-warning mx-auto mt-2"> 
+                        </tr>
 
-                  {{__('ui.run')}} {{$en['status']}}    
-              </h4>
-            </div>
-                
-            @endif
-        <div class="row d-flex justify-content-center mt-1 ">
-            <input type="hidden" name="status"  value="{{$en->status}}"/>
 
+
+                        
+                    <tr>
+                        <td> <label class=" og  w-100  "> <i class="fas fa-cog "> &nbsp  {{__('ui.en6')}}</i>  </td>
+                        <td>  <input type="range" name="engineN6" class="pointss w-100 "  value={{$en->engineN6}}
+                          oninput="changevalue3(5); " min="0" max="180">  </td>
+                        <td>             <label class="ps3 og   w-100     text-light"value={{$en->engineN6}}>{{$en->engineN6}}</label>
+                        </td>
           
+                        </tr>
 
-                <button type="submit" class="btn btn-primary m-1 col-md-2      btn-sm border border-light border-3  h-50">
+
+
+                        
+
+
+
+
+
+
+                        <input type="hidden" name="lang" value='{{__('ui.lang')}}'/>
+
+             </table>
+             <button type="submit" class="btn btn-primary  col-md-2      btn-sm border border-light border-3  h-50">
+                <span>
+                    <i class=" fas fa-solid fa-server text-light p-1"> {{__('ui.saveBtn')}} </i>
+                </span>
+            </button>
+          
+         
+                <button type="submit" name="st"  value="{{$en->status}}"
+                    class="btn btn-warning col-md-2 btn-sm border border-warning border-3 h-50">
+    
                     <span>
-                        <i class=" fas fa-solid fa-server text-light p-1"> {{__('ui.saveBtn')}} </i>
+                        <i class="fa-solid fas fa-play text-dark p-1">
+                            {{__('ui.runBtn')}}
+                            @if ($en->status=='on')
+                               {{'OFF'}}
+                            @else
+                            {{'ON'}}
+
+                            @endif
+                        </i>
+    
                     </span>
                 </button>
-              
-             
-                    <button type="submit" name="st"  value="{{$en->status}}"
-                        class="btn btn-warning mb-4  m-1  col-md-2 btn-sm border border-warning border-3 h-50">
-        
-                        <span>
-                            <i class="fa-solid fas fa-play text-dark p-1">
-                                {{__('ui.runBtn')}}
-                                @if ($en->status=='on')
-                                   {{'OFF'}}
-                                @else
-                                {{'ON'}}
-    
-                                @endif
-                            </i>
-        
-                        </span>
-                    </button>
 
-                
+         </div>
 
-                 
-        </div>
-
-                    
-          
-             
-            
-    
-
-    </div>
-     
+         
         </form>
+      
+
+    
+
 
 
 
